c11132c50b54e85570bbae949eaf9a95149c6798
Syed Ali Haider <s.haiderali2025@gmail.com>
2025-05-26 07:15:29 +0000

    container config down

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//Treat all ja code as newer version
+// alert(3+3) we are useing nodejs no browser
+// console.log(3+3);console.log("Shah") True method but ues next method
+console.log(3+3);
+console.log("SHAH");
+let name=("SHAH");
+let age =(18);
+let isLoggenIn= false
+//DTATatypes are follawing
+/* number =>2 to power 53
+bigint (GREATER TYPE OF NUMBER)
+string =""   datattyes
+boolearn =true/false
+null=> standalone value or object
+undifined => datatype 
+symbole=> unique
+*/
+//Object
+console.log(typeof "SHAh");
+console.log(typeof age);
+console.log (typeof null);
+console.log(typeof undifined);
+console.log(typeof isLoggenIn);
+console.log(typeof 18);
